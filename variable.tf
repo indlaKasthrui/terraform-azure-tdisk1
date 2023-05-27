25297f31d0fe6404e68e11a372fe9d784ffa9f5a
kasthuri indla <122959813+indlaKasthrui@users.noreply.github.com>
2023-05-27 06:56:23 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,13 @@
+
+variable "diskname" {
+    default = "testdisk0"
+}
+variable "acc_type" {
+    default = "Standard_LRS"
+}
+variable "create" {
+    default = "Empty"
+}
+variable "size" {
+    default = "10"
+}
